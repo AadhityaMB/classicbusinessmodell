dd48c58c2250c681e17f6c28f013b57cd03c9e5a
Harini <hariniichandran2905@gmail.com>
2026-04-15 10:35:21 +0530

    Changed

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/employee/repository/EmployeeRepository.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/employee/repository/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.classicbusinessmodel_schema.backend.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
 }
--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/employee/repository/OfficeRepository.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/employee/repository/OfficeRepository.java
@@ -4,4 +4,6 @@ import com.classicbusinessmodel_schema.backend.entity.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfficeRepository extends JpaRepository<Office, String> {
+
+
 }
