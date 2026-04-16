c3911955d392ac19fc87bdd2a69a0fd821520a6f
Pritika <pritikasridhar2004@gmaail.com>
2026-04-16 09:36:53 +0530

    Order service added

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/orders/service/OrdersService.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/orders/service/OrdersService.java
@@ -1,4 +1,18 @@
 package com.classicbusinessmodel_schema.backend.module.orders.service;
 
 public interface OrdersService {
+
+    Orders createOrder(Orders order);
+
+    List<Orders> getAllOrders();
+
+    Orders getOrderById(Integer orderNumber);
+
+    Orders updateOrder(Integer orderNumber, Orders order);
+
+    Orders updateOrderStatus(Integer orderNumber, String status);
+
+    List<Orders> getOrdersByCustomer(Integer customerNumber);
+
+    List<Orders> searchOrders(String status, LocalDate fromDate, LocalDate toDate);
 }
