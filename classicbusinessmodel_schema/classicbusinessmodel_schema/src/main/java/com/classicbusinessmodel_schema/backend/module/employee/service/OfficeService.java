8a96a26b3a3064de6c9819cf0e8d8ed2f3c29d1d
Harini <hariniichandran2905@gmail.com>
2026-04-16 09:37:10 +0530

    Service and Service implementations added

---
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/employee/service/OfficeService.java
@@ -0,0 +1,14 @@
+package com.classicbusinessmodel_schema.backend.module.employee.service;
+
+import com.classicbusinessmodel_schema.backend.entity.Office;
+
+import java.util.List;
+
+public interface OfficeService {
+
+    List<Office> getAllOffices();
+
+    Office getOfficeByCode(String officeCode);
+
+    List<?> getEmployeesByOffice(String officeCode);
+}
