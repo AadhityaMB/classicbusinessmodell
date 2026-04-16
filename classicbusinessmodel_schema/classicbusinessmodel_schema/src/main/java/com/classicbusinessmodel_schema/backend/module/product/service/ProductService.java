3d85e66df313ff171cfaba4f0a39813de89011f6
mk-codes7 <mkdevcodehq@gmail.com>
2026-04-16 10:23:27 +0530

    Added product module services

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/product/service/ProductService.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/product/service/ProductService.java
@@ -1,4 +1,13 @@
 package com.classicbusinessmodel_schema.backend.module.product.service;
 
+import com.classicbusinessmodel_schema.backend.entity.Product;
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    Product getProductById(String productCode);
+
+    List<Product> getProductsByLine(String productLine);
 }
--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/product/service/ProductServiceImpl.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/product/service/ProductServiceImpl.java
@@ -1,4 +1,33 @@
 package com.classicbusinessmodel_schema.backend.module.product.service;
 
-public class ProductServiceImpl {
+import com.classicbusinessmodel_schema.backend.entity.Product;
+import com.classicbusinessmodel_schema.backend.exception.ResourceNotFoundException;
+import com.classicbusinessmodel_schema.backend.module.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductServiceImpl(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Product getProductById(String productCode) {
+        return repository.findById(productCode)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    @Override
+    public List<Product> getProductsByLine(String productLine) {
+        return repository.findByProductLineProductLine(productLine);
+    }
 }
