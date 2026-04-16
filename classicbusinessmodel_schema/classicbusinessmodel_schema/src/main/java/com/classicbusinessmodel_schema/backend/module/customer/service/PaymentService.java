7f07d99d6f2e398b021b0303340e36f836655e4d
Priya <priyaofficial004@gmail.com>
2026-04-16 11:10:06 +0530

    added customer service

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/customer/service/PaymentService.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/module/customer/service/PaymentService.java
@@ -1,4 +1,14 @@
 package com.classicbusinessmodel_schema.backend.module.customer.service;
 
+
+import com.classicbusinessmodel_schema.backend.entity.Payment;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PaymentService {
+
+    List<Payment> getPaymentsByCustomer(Integer customerId);
+
+    List<Payment> getPaymentsBetweenDates(LocalDate start, LocalDate end);
 }
