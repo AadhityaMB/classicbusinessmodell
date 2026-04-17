ee7c6f9ebfc5071316b7b29bb99f8392e84f3b0b
Harini <hariniichandran2905@gmail.com>
2026-04-17 09:32:19 +0530

    Controllers and DTOs are added in Employee module

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/entity/Office.java
+++ classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/entity/Office.java
@@ -2,55 +2,151 @@ package com.classicbusinessmodel_schema.backend.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
 @Table(name = "offices")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Office {
 
         @Id
         @Column(name = "officeCode")
         private String officeCode;
 
-        @NotBlank
-        @Size(max = 50)
+        @Column(length = 50)
         private String city;
 
-        @NotBlank
-        @Size(max = 50)
+        @Column(length = 50)
         private String phone;
 
-        @NotBlank
-        @Size(max = 50)
+        @Column(length = 50)
         private String addressLine1;
 
-        @Size(max = 50)
+        @Column(length = 50)
         private String addressLine2;
 
-        @Size(max = 50)
+        @Column(length = 50)
         private String state;
 
-        @NotBlank
-        @Size(max = 50)
+        @Column(length = 50)
         private String country;
 
-        @NotBlank
-        @Size(max = 15)
+        @Column(length = 15)
         private String postalCode;
 
-        @NotBlank
-        @Size(max = 10)
+        @Column(length = 10)
         private String territory;
 
         @JsonIgnore
         @OneToMany(mappedBy = "office", fetch = FetchType.LAZY)
-        private List<Employee> employees;
+        private List<Employee> employees = new ArrayList<>();
+
+        public Office() {
+        }
+
+        public Office(String officeCode, String city, String phone, String addressLine1,
+                      String addressLine2, String state, String country,
+                      String postalCode, String territory, List<Employee> employees) {
+                this.officeCode = officeCode;
+                this.city = city;
+                this.phone = phone;
+                this.addressLine1 = addressLine1;
+                this.addressLine2 = addressLine2;
+                this.state = state;
+                this.country = country;
+                this.postalCode = postalCode;
+                this.territory = territory;
+                this.employees = employees;
+        }
+
+        public String getOfficeCode() {
+                return officeCode;
+        }
+
+        public void setOfficeCode(String officeCode) {
+                this.officeCode = officeCode;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        public String getPhone() {
+                return phone;
+        }
+
+        public void setPhone(String phone) {
+                this.phone = phone;
+        }
+
+        public String getAddressLine1() {
+                return addressLine1;
+        }
+
+        public void setAddressLine1(String addressLine1) {
+                this.addressLine1 = addressLine1;
+        }
+
+        public String getAddressLine2() {
+                return addressLine2;
+        }
+
+        public void setAddressLine2(String addressLine2) {
+                this.addressLine2 = addressLine2;
+        }
+
+        public String getState() {
+                return state;
+        }
+
+        public void setState(String state) {
+                this.state = state;
+        }
+
+        public String getCountry() {
+                return country;
+        }
+
+        public void setCountry(String country) {
+                this.country = country;
+        }
+
+        public String getPostalCode() {
+                return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+                this.postalCode = postalCode;
+        }
+
+        public String getTerritory() {
+                return territory;
+        }
+
+        public void setTerritory(String territory) {
+                this.territory = territory;
+        }
+
+        public List<Employee> getEmployees() {
+                return employees;
+        }
+
+        public void setEmployees(List<Employee> employees) {
+                this.employees = employees;
+        }
+
+        @Override
+        public String toString() {
+                return "Office{" +
+                        "officeCode='" + officeCode + '\'' +
+                        ", city='" + city + '\'' +
+                        ", phone='" + phone + '\'' +
+                        ", country='" + country + '\'' +
+                        '}';
+        }
 }
