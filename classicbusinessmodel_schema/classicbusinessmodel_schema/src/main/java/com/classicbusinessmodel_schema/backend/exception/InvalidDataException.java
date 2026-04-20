fb20a0dcd0ee08eee82890711fee0b6cad118c35
Aadhitya M B <aadhityamb@gmail.com>
2026-04-20 14:21:26 +0530

    Added FrontEnd and removed additional folder

--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/exception/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.classicbusinessmodel_schema.backend.exception;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
--- classicbusinessmodel_schema/classicbusinessmodel_schema/src/main/java/com/classicbusinessmodel_schema/backend/exception/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.classicbusinessmodel_schema.backend.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
